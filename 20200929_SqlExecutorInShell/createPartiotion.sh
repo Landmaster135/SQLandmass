101e7755dd751345434511f0861002117c04c3e2
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-10-02 20:03:40 +0900

    Rename createPartiotion.sql to createPartiotion.sh

--- 20200929_SqlExecutorInShell/createPartiotion.sh
+++ 20200929_SqlExecutorInShell/createPartiotion.sh
@@ -6,7 +6,7 @@ SHNAME=`basename $0`
 echo [$SHNAME] start time >> $TARGETDIR/timestamp.txt
 date >> $TARGETDIR/timestamp.txt
 
-hdbsql -U PIOAPKEY -I $TARGETDIR/insertPartition.sql
+hdbsql -U PIOAPKEY -I $TARGETDIR/createPartiotion.sql
 
 echo [$SHNAME] end time >> $TARGETDIR/timestamp.txt
 date >> $TARGETDIR/timestamp.txt
