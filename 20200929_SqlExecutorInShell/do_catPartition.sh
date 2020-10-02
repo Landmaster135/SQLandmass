d9b8da8934f9bd905a72dac2efab3e9bf6cbc765
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-10-02 21:24:56 +0900

    Update do_catPartition.sh

--- 20200929_SqlExecutorInShell/do_catPartition.sh
+++ 20200929_SqlExecutorInShell/do_catPartition.sh
@@ -7,7 +7,14 @@ echo [$SHNAME] start time >> $TARGETDIR/timestamp.txt
 date >> $TARGETDIR/timestamp.txt
 
 
-cat $TARGETDIR/index/PIOAP/_S/_SYS_SPLIT_CONSULT_CM_TMP*/data.csv>  $TARGETDIR/CONSULT_CM_allRecord.csv
+cat $TARGETDIR/index/WARDROBE_AP/_S/_SYS_SPLIT_WARDROBE_INFO_TMP*/data.csv> $TARGETDIR/WARDROBE_INFO_allRecord.csv
+
+cat $TARGETDIR/index/WARDROBE_AP/_S/_SYS_SPLIT_WARDROBE_INFO_TMP*/data.csv> $TARGETDIR/WEATHER_INFO_allRecord.csv
+
+
+cat $TARGETDIR/index/WARDROBE_AP/_S/_SYS_SPLIT_WARDROBE_INFO_TMP*/data.csv> $TARGETDIR/USER_INFO_allRecord.csv
+
+cat $TARGETDIR/index/WARDROBE_AP/_S/_SYS_SPLIT_WARDROBE_INFO_TMP*/data.csv> $TARGETDIR/SEARCH_COUNT_FOR_CC_INFO_ALL_allRecord.csv
 
 
 echo [$SHNAME] end time >> $TARGETDIR/timestamp.txt
